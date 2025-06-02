ae3edee19b62c82463d66e5ad753e5f3fe5b4d3d
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-06-02 11:58:58 +0200

    generic bootstrap navbar with dark theme added with link to loggin

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,13 @@ import App from "./App.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalProvider } from "./components/context.jsx";
 import { CookiesProvider } from "react-cookie";
+import NavBar from "./components/NavBar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <GlobalProvider>
       <CookiesProvider>
+        <NavBar/>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/app" element={<App />} />
