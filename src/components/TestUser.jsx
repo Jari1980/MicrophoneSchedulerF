ed15625c86ce2cef44e36e8226d1b227e4c1a4f1
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-06-04 09:12:42 +0200

    Moved username and role to cookie, if logged in name and logout appears in navbar else login, after login redirecting to root

--- src/components/TestUser.jsx
+++ src/components/TestUser.jsx
@@ -6,14 +6,21 @@ import { useCookies } from "react-cookie";
 const TestUser = () => {
   const { userName, setUserName } = useGlobalContext();
   const { userRole, setUserRole } = useGlobalContext();
-  const [cookies, setCookie] = useCookies(["jwtToken"]);
+  const [cookies, setCookie] = useCookies(["jwtToken", "userName", "userRole"]);
 
   return (
     <>
       <Container>
+        <br />
+        <h1>From GlobalContext: </h1>
         <h2>User: {userName}</h2>
         <h2>UserRole: {userRole}</h2>
+        <br />
+        <br />
+        <h1>From Cookie</h1>
         <h2>JWT: {cookies.jwtToken}</h2>
+        <h2>User: {cookies.userName}</h2>
+        <h2>UserRole: {cookies.userRole}</h2>
       </Container>
     </>
   );
