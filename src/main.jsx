45566664405933b4e8fbebfde3c0e13ed08afebe
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-06-05 14:18:24 +0200

    component get users and roles added, need to add functionality to edit and delete

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import { CookiesProvider } from "react-cookie";
 import NavBar from "./components/NavBar.jsx";
 import LogIn from "./components/LogIn.jsx";
 import TestUser from "./components/TestUser.jsx";
+import UserService from "./components/userService.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -18,6 +19,7 @@ createRoot(document.getElementById("root")).render(
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/home" element={<App />} />
+          <Route path="/userservice" element={<UserService />} />
           <Route path="/testuser" element={<TestUser />} />
           <Route path="/login" element={<LogIn />} />
         </Routes>
