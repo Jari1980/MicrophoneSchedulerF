d241bce0c3e1d36aeb6627087bb493e70b2d2484
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-06-05 15:45:35 +0200

    delete and edit role working

--- src/components/UserService.jsx
+++ src/components/UserService.jsx
@@ -42,16 +42,55 @@ const UserService = () => {
     }
   };
 
-  function deleteUser(userId){
-    console.log(userId)
+  function deleteUser(userId) {
+    try {
+      const response = axios.delete(
+        `http://localhost:8080/api/v1/admin/deleteuser?userId=${userId}`,
+        {
+          headers: { Authorization: `Bearer ${cookies.jwtToken}` },
+        }
+      )
+      .then(
+        fetchData,
+        console.log("mmmm")
+      )
+      
+    } catch (error) {
+      console.log("error deleting: " + error);
+    }
+    console.log(userId);
   }
 
-  
-  function editUser(userId, role){
-    console.log(userRole)
+  function editUser(userId, role) {
+    try {
+      const response = axios
+        .post(
+          "http://localhost:8080/api/v1/admin/setuserrole",
+          {
+            userId: userId,
+            userRole: role,
+          },
+          {
+            headers: { Authorization: `Bearer ${cookies.jwtToken}` },
+          }
+        )
+        .then(console.log("Role edited"));
+    } catch (error) {
+      console.log("eroor editing role: " + error);
+    }
   }
 
-
+  //Function to uppdate userData with new role
+  function uppdate(id, newRole) {
+    console.log(id + role);
+    const newUserData = userData.map((item) => {
+      if (item.userId == id) {
+        return { ...item, role: newRole };
+      }
+      return item;
+    });
+    setUserData(newUserData);
+  }
 
   return (
     <>
@@ -73,9 +112,10 @@ const UserService = () => {
               <td>{item.userName}</td>
               <td>
                 <select
-                  id="roleSet"
+                  id="role"
                   defaultValue={item.role}
                   className={`form-select`}
+                  onChange={(event) => uppdate(item.userId, event.target.value)}
                 >
                   {roles.map((type) => (
                     <option key={type.id} value={type.role}>
@@ -85,12 +125,10 @@ const UserService = () => {
                 </select>
               </td>
               <td>
-                <Button onClick={() => editUser(item.userId, item.userRole)}>
+                <Button onClick={() => editUser(item.userId, item.role)}>
                   Edit Role
                 </Button>
-                <Button onClick={() => deleteUser(item.userId)}>
-                  Delete
-                </Button>
+                <Button onClick={() => deleteUser(item.userId)}>Delete</Button>
               </td>
             </tr>
           </tbody>
