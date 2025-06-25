2e4937455feb097b55fc4018574d339425991702
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-06-25 12:26:00 +0200

    users can view each others schedules

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,44 +1,82 @@
 import React from "react";
-import { useState } from 'react'
-import './App.css'
-import { Button } from 'react-bootstrap'
+import { useState } from "react";
+import "./App.css";
+import { Button } from "react-bootstrap";
 import { useCookies } from "react-cookie";
 import CookieConsent from "./components/CookieConsent";
 import { redirect, useNavigate } from "react-router-dom";
 import { useGlobalContext } from "./components/context";
 
 function Home() {
-  const [cookies, setCookie] = useCookies(["cookieConsent", "jwtToken", "userName", "userRole"])
+  const [cookies, setCookie] = useCookies([
+    "cookieConsent",
+    "jwtToken",
+    "userName",
+    "userRole",
+  ]);
   const navigate = useNavigate();
-  const {bgColor, setBgColor} = useGlobalContext();
+  const { bgColor, setBgColor } = useGlobalContext();
 
   return (
     <>
-    <div style={{backgroundImage:bgColor, width:"100vw", height:"100vh", overflow: "hidden"}}>
-      {!cookies.cookieConsent && <CookieConsent />}
-      <br />
-      <div style={{textAlign:"center"}}>
-      <h1 style={{fontFamily: "Brush Script MT", fontSize: "44px"}}>Teaterstickorna</h1>
-      <br/>
-      <b>Image?</b>
-      <br />
-      <br />
-      <br />
-      {cookies.userRole == "ROLE_ADMINISTRATOR" || cookies.userRole == "ROLE_DIRECTOR" ? 
-      <Button style={{width: "240px"}} onClick={() => navigate("/dashboard")}>Manage Theater productions</Button>
-       : ""}
-      <br />
-      <br />
-      {cookies.userRole == "ROLE_ADMINISTRATOR" ? 
-      <Button style={{width: "240px"}} onClick={() => navigate("/userservice")}>User Service</Button> 
-      : ""}
-      {cookies.userRole == "ROLE_ACTOR" ?
-      <Button style={{width: "240px"}} onClick={() => navigate("/actorscenes")}>Microphone Schedule</Button>
-    :""}
-      </div>
+      <div
+        style={{
+          backgroundImage: bgColor,
+          width: "100vw",
+          height: "100vh",
+          overflow: "hidden",
+        }}
+      >
+        {!cookies.cookieConsent && <CookieConsent />}
+        <br />
+        <div style={{ textAlign: "center" }}>
+          <h1 style={{ fontFamily: "Brush Script MT", fontSize: "44px" }}>
+            Teaterstickorna
+          </h1>
+          <br />
+          <b>Image?</b>
+          <br />
+          <br />
+          <br />
+          {cookies.userRole == "ROLE_ACTOR" || cookies.userRole == "ROLE_ADMINISTRATOR"|| cookies.userRole == "ROLE_DIRECTOR"? (
+            <Button
+              style={{ width: "240px" }}
+              onClick={() => navigate("/actorscenes")}
+            >
+              View Microphone Schedule
+            </Button>
+          ) : (
+            ""
+          )}
+          <br />
+          <br />
+          {cookies.userRole == "ROLE_ADMINISTRATOR" ||
+          cookies.userRole == "ROLE_DIRECTOR" ? (
+            <Button
+              style={{ width: "240px" }}
+              onClick={() => navigate("/dashboard")}
+            >
+              Manage Theater productions
+            </Button>
+          ) : (
+            ""
+          )}
+          <br />
+          <br />
+          {cookies.userRole == "ROLE_ADMINISTRATOR" ? (
+            <Button
+              style={{ width: "240px" }}
+              onClick={() => navigate("/userservice")}
+            >
+              User Service
+            </Button>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Home;
