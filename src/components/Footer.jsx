0110efa56f496eda0bcb2979181bd97601b8e1b9
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-07-02 11:09:08 +0200

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Container, Col, Stack, Nav, Button, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { Link } from "react-router-dom";
+import { useGlobalContext } from "./context";
+
+const Footer = () => {
+  const [cookies, setCookie] = useCookies(["jwtToken", "userName", "userRole"]);
+  const navigate = useNavigate();
+  const {footerRowClass, setFooterRowClass} = useGlobalContext();
+  const {footerLinkText, setFooterLinkText} = useGlobalContext();
+
+  function logout(){
+        setCookie("jwtToken", "", {path: "/"})
+        setCookie("userName", "", {path: "/"})
+        setCookie("userRole", "", {path: "/"})
+        navigate("/")
+    }
+
+  return (
+    <footer>
+      <Container fluid>
+        <Row className={footerRowClass}>
+          <Col className="mx-4">
+            <Stack>
+              <h3>Teaterstickorna</h3>
+              <p>Microphone Scheduler</p>
+              <p></p>
+            </Stack>
+          </Col>
+          <Col>
+            <Nav className="flex-column fs-5">
+              <h5>Short Links</h5>
+              <Nav.Link className={footerLinkText} as={Link} to="/home">
+                Home
+              </Nav.Link>
+              <Nav.Link className={footerLinkText} as={Link} to="/home">
+              About
+            </Nav.Link>
+              {cookies.userName == "" || cookies.userName == null ? (
+                <Nav.Link className={footerLinkText} as={Link} to="/register">
+                  Register
+                </Nav.Link>
+              ) : (
+                ""
+              )}
+              {cookies.userName == "" || cookies.userName == null ? (
+                <Nav.Link className={footerLinkText} as={Link} to="/login">
+                  Login
+                </Nav.Link>
+              ) : (
+                <Nav.Link className={footerLinkText} onClick={() => logout()}>
+                  {cookies.userName} SignOut
+                </Nav.Link>
+              )}
+            </Nav>
+          </Col>
+          <Col>
+          <h5>Contact:</h5>
+          <p>???</p>
+          <p>???</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,6 +12,8 @@ const NavBar = () => {
     const {bgColor, setBgColor} = useGlobalContext();
     const {bgColorDashboard, setBgColorDashboard} = useGlobalContext();
     const {dashboardText, setDashboardText} = useGlobalContext();
+    const {footerRowClass, setFooterRowClass} = useGlobalContext();
+    const {footerLinkText, setFooterLinkText} = useGlobalContext();
 
     function logout(){
         setCookie("jwtToken", "", {path: "/"})
@@ -26,12 +28,16 @@ const NavBar = () => {
       setBgColor("linear-gradient(120deg,rgb(117, 143, 231), rgb(149, 165, 207) 50%,rgb(221, 224, 233))")
       setBgColorDashboard("linear-gradient(120deg,rgb(117, 143, 231), rgb(149, 165, 207) 50%,rgb(221, 224, 233))")
       setDashboardText("indigo")
+      setFooterRowClass("bg-white text-black p-2")
+      setFooterLinkText("text-black")
     }
     else{
       setDark("dark")
       setBgColor("linear-gradient(120deg,rgb(114, 119, 138),rgb(71, 97, 190) 50%,rgb(132, 142, 173))")
       setBgColorDashboard("linear-gradient(120deg,rgb(114, 119, 138),rgb(74, 85, 126) 50%,rgb(135, 138, 148))")
       setDashboardText("whitesmoke")
+      setFooterRowClass("bg-dark text-secondary p-2")
+      setFooterLinkText("text-secondary")
     }
   }
 
